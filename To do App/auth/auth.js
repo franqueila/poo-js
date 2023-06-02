26d4908c7f5f72a50005fc9068e033c09be6a5ef
franqueila <100473332@est.uasd.edu.do>
2023-06-02 02:07:57 -0400

    antes de iniciar npm

--- To do App/auth/auth.js
+++ To do App/auth/auth.js
@@ -1,3 +1,6 @@
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+console / log(supabase);
+
 function signInForm() {
     document.getElementById("formTitle").innerText = "Iniciar Sesión";
     const authForm = document.getElementById("authForm");
@@ -10,75 +13,66 @@ function signInForm() {
     `;
 
     const questionBtn = document.getElementById("questionBtn");
-    questionBtn.addEventListener("click", signUpForm);
+    questionBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        signUpForm();
+    });
 }
 
 function signUpForm() {
     document.getElementById("formTitle").innerText = "Registrarse";
     const authForm = document.getElementById("authForm");
     authForm.innerHTML = `
-        <input type="text" class="form-control text-center mb-3" id="name" placeholder="Nombre">
         <input type="email" class="form-control text-center mb-3" id="email" placeholder="Correo electrónico">
         <input type="password" class="form-control text-center mb-3" id="password" placeholder="Contraseña">
+        <input type="password" class="form-control text-center mb-3" id="passwordConfirm" placeholder="Confirmar contraseña">
         <button class="btn btn-primary btn-block" type="submit" id="signUpBtn">Enviar</button>
         <hr class="mb-3">
         <p class="text-body-secondary">¿Ya tienes una cuenta? <a href="#" id="questionBtn"><strong>Iniciar sesión</strong></a></p>
     `;
 
     const questionBtn = document.getElementById("questionBtn");
-    questionBtn.addEventListener("click", signInForm);
+    questionBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        signInForm();
+    });
 }
 
 signInForm();
 
-/*
-document.getElementById("signUpBtn").addEventListener("click", (e) => {
+document.getElementById("authForm").addEventListener("submit", (e) => {
     e.preventDefault();
+    if (e.submitter.id === "signInBtn") {
+        signIn();
+    } else if (e.submitter.id === "signUpBtn") {
+        signUp();
+    }
 });
 
-class Auth {
-    constructor() {
-        this.auth = firebase.auth();
+function signUp() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const passwordConfirm = document.getElementById("passwordConfirm").value;
+
+    if (email === "" || password === "" || passwordConfirm === "") {
+        console.log("Campos vacíos");
+        return;
     }
-    login(email, password) {
-        this.auth
-            .signInWithEmailAndPassword(email, password)
-            .then((user) => {
-            console.log("Logueado");
-            window.location.href = "index.html";
-        })
-            .catch((error) => {
-            console.log(error);
-        });
-    }
-}
-*/
-/*class AuthForm {
-    constructor() {
-        this.authForm = document.getElementById("authForm");
-        this.authForm.addEventListener("submit", (e) => {
-            e.preventDefault();
-            const email = document.getElementById("email").value;
-            const password = document.getElementById("password").value;
-            const auth = new Auth();
-            auth.login(email, password);
-        });
+
+    if (password === passwordConfirm) {
+        //Registrarse con supabase
+    } else {
+        console.log("Contraseñas no coinciden");
     }
 }
-class Auth {
-    constructor() {
-        this.auth = firebase.auth();
+
+function signIn() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "" || password === "") {
+        console.log("Campos vacíos");
+        return;
     }
-    login(email, password) {
-        this.auth
-            .signInWithEmailAndPassword(email, password)
-            .then((user) => {
-            console.log("Logueado");
-            window.location.href = "index.html";
-        })
-            .catch((error) => {
-            console.log(error);
-        });
-    }
+    //logease con supabase
 }
-*/
